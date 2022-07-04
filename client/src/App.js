8b4b7063f34ceeda3b421bf597cf28c29a210bee
sudipto mondal <msudipto035@gmail.com>
2022-07-04 23:54:50 +0530

    Add profile component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route , Routes , BrowserRouter, Navigate} from 'react-router-dom'
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import AddNews from './pages/AddNews'
 import LandingPage from './pages/LandingPage'
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 import NewsDesc from './pages/NewsDesc';
 import PostedNewsItems from './pages/PostedNewsItems';
 import EditNews from './pages/EditNews';
-
+import Profile from './pages/Profile'
 
 function App() {
-  
+
   return (
     <div className="App">
-       <ToastContainer />
-       <BrowserRouter>
-          <Routes>
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
           <Route path='/' element={<LandingPage />} />
-            <Route path='/home' element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-            <Route path='/add' element={<ProtectedRoute><AddNews/></ProtectedRoute>} />
-            <Route path='/edit/:newsid' element={<ProtectedRoute><EditNews/></ProtectedRoute>} />
-            <Route path='/posted' element={<ProtectedRoute><PostedNewsItems /></ProtectedRoute>} />
-            <Route path='/newsdesc/:newsid' element={<ProtectedRoute><NewsDesc/></ProtectedRoute>} />
-          </Routes>
-       </BrowserRouter>
+          <Route path='/home' element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+          <Route path='/add' element={<ProtectedRoute><AddNews /></ProtectedRoute>} />
+          <Route path='/edit/:newsid' element={<ProtectedRoute><EditNews /></ProtectedRoute>} />
+          <Route path='/posted' element={<ProtectedRoute><PostedNewsItems /></ProtectedRoute>} />
+          <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path='/newsdesc/:newsid' element={<ProtectedRoute><NewsDesc /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
@@ -33,12 +34,12 @@ function App() {
 export default App;
 
 
-export const ProtectedRoute=({children})=>{
+export const ProtectedRoute = ({ children }) => {
 
-  if(localStorage.getItem('sheynews-user')){
-        return children
-  }else{
-      return <Navigate to='/' />
+  if (localStorage.getItem('sheynews-user')) {
+    return children
+  } else {
+    return <Navigate to='/' />
   }
 
 }
